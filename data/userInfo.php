61aa2980f67f175f21ba73bc081725b0cd2a67b2
Eduard Muliar <edikmulyar228@gmail.com>
2021-11-09 18:32:27 +0200

    Added new files 'style.css', 'userInfo.php' and made output a table with info

---
+++ data/userInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+$users = [
+    ['PIB'=>'Муляр Едуард Русланович', 'age'=>35, 'education'=>'Вища','experience'=>1],
+    ['PIB'=>'Власюк Микола Ігорович', 'age'=>30, 'education'=>'Середня','experience'=>4],
+    ['PIB'=>'Стасенко Влад Вадимович', 'age'=>17, 'education'=>'Вища','experience'=>1],
+    ['PIB'=>'Лєдяєва Катерина Василівна', 'age'=>24, 'education'=>'Середня','experience'=>2],
+    ['PIB'=>'Гончарук Віталіна Миколаїівна', 'age'=>22, 'education'=>'Вища','experience'=>2],
+    ['PIB'=>'Сороченко Андрій Іванович', 'age'=>28, 'education'=>'Вища','experience'=>5],
+];
+
+sort($users);
--- index.php
+++ index.php
@@ -1,12 +1,60 @@
+<?php include('data/userInfo.php');
+$className = '';
+$classAge = '';
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHPTask1</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    
+<div class="main__block">
+    <table class="main__table">
+        <tr>
+            <th>ПIБ</th>
+            <th>Вiк</th>
+            <th>Освiта</th>
+            <th>Досвiд</th>
+        </tr>
+        <?php foreach ($users as $key => $user):?>
+        
+            <?php if($user['education'] == 'Вища'):?>
+               <?php $className .= "education "?>
+            <?php endif;?>
+
+            <?php if($user['experience'] < 3):?>
+               <?php $className .= "greenBackground "?>
+            <?php endif;?>
+
+            <?php if($user['age'] < 18):?>
+               <?php $classAge .= "ageFontLess"?>
+            <?php elseif($user['age'] >= 30):?> 
+                <?php $classAge .= "ageFontMore"?>
+            <?php else:?>
+                <?php $classAge .= "ageFontMiddle"?>
+            <?php endif;?>
+
+            <tr>
+                   
+                <?php foreach($user as $key=> $value):?>
+                    <?php if($key == 'age'):?>
+                        <td class="<?php echo $className.' '.$classAge;?>"><?php echo $value;?></td>
+                    <?php else:?>   
+                        <td class="<?php echo $className;?>"><?php echo $value;?></td>
+                    <?php endif;?>
+                       
+                <?php endforeach;?>  
+                
+            </tr>
+            <?php $className = '';?>
+            <?php $classAge = '';?>
+        <?php endforeach;?>
+        
+    </table>
+   </div>
 </body>
 </html>
